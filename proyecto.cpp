ea3ea5230625907fbce2f6a2ddd66a2c1c2c288a
Angel Rodriguez <angelemmanuelrodriguezdiaz95@gmail.com>
2024-12-10 21:29:10 -0600

    Borrador 2

--- proyecto.cpp
+++ proyecto.cpp
@@ -3,154 +3,124 @@
 
 #define NOMBRE_VALIDO "AngelRdz"
 #define NUMERO_DE_TARJETA_VALIDO "226699"
-#define PIN_VALIDO "1234"
-#define SALDO_INICIAL 10000
+#define PIN_VALIDO 1234
 
+int pin_valido = PIN_VALIDO;
+float saldo = 10000;
 
-void capturarDatos(char nombre[], char numeroTarjeta[], int pin, int i){
-    
+void capturarDatos(char nombre[], char numeroTarjeta[], int pin) {
     //Capturamos los datos del usuario
     printf("Por favor ingrese su nombre: ");
     scanf("%s", nombre);
 
-    // Damos la bienvenida al usuario
+    //Bienvenida al usuario
     printf("-------Bienvenido a CodexBank-------\n");
     printf("\n\nDonde sus bits generan ganancias\n\n");
     printf("\t");
     printf("Buenas tardes %s\n", nombre);
 
-    //Capturamos el número de tarjeta
+    //Capturaremos el numero de tarjeta
     printf("Por favor %s ingrese su numero de tarjeta: ", nombre);
     scanf("%s", numeroTarjeta);
 
-    //Capturamos el pin
+    //Capturaremos el pin
     printf("Por favor ingrese su pin: ");
     scanf("%d", &pin);
 
-    /*Tendremos que convertir el pin a cadena para poder compararlo en la funcion strcmp,
-    ya que solo admite cadenas y nosotros tenemos un entero, para eso usaremos una funcion llamada
-    "sprintf", la cual no imprime un valor directamente en la consola si no que devuelve la cadena*/
+    //Comprobaremos si los datos ingresados son correctos
+    if (strcmp(numeroTarjeta, NUMERO_DE_TARJETA_VALIDO) == 0 || pin == pin_valido) {
+        printf("Datos correctos. Bienvenido %s\n", nombre);
 
-    char pinCadena[5];
-    sprintf(pinCadena, "%d", pin);
-
-    //Dar un maximo de 3 intentos
-
-    // Comprobaremos que los datos sean correctos
-    for (int i = 0; i < 3; i++)
-    {
-        if (strcmp(numeroTarjeta, NUMERO_DE_TARJETA_VALIDO) == 0 && strcmp(pinCadena, PIN_VALIDO) == 0){
-        printf("Los datos ingresados son correctos, bienvenido nuevamente %s\n", nombre, i++);
-        printf("Cargando menu de opciones...\n");
-
-        }else
-        {
-        printf("Los datos ingresados son incorrectos, por favor intente nuevamente.\n");
-        }
+    } else {
+        printf("Datos incorrectos. Por favor verifique su informacion\n");
     }
-    
 }
 
-void mostrarMenu(int opcion, float retiro, float saldoActual, int masOpciones) {
-    printf("----Ingrese el numero de opcion que desee realizar---- \n");
-    printf("1. Consultar su saldo\n");
-    printf("2. Mostrar las transacciones recientes\n");
-    printf("3. Retirar dinero\n");
-    printf("4. Salir y cerrar sesion\n");
-    printf("5. Mas opciones\n");
-    scanf("%d", &opcion);
+void consultarSaldo() {
+}
 
-    switch (opcion) {
-        case 1:
-            printf("----Consulta de saldo----\t");
-            printf("Su saldo actual es de: %0.2f\n", SALDO_INICIAL);
-            break;
+void mostrarTransaccionesRecientes() {
 
-        case 2:
-            printf("----Mostrar transacciones recientes----\n");
+}
 
-            break;
+void retirarDinero() {
 
-        case 3:
-            printf("----Retirar dinero----\t");
-            printf("Ingrese la cantidad  de dinero que desea retirar, usted cuenta con: %f\n", SALDO_INICIAL);
-            scanf("%f", &retiro);
+}
 
-            retiro -= SALDO_INICIAL;
-            saldoActual = SALDO_INICIAL - retiro;
+void mostrarMenu(int opcion, int masOpciones,int nuevoPin, char numeroTelefono[]) {
 
-            printf("Usted ha retirado: %0.2f\n, y su saldo actual es de %0.2f\n ", retiro, saldoActual);
+    do {
+        printf("----Ingrese el numero de opcion que desee realizar---- \n");
+        printf("1. Consultar su saldo\n");
+        printf("2. Mostrar las transacciones recientes\n");
+        printf("3. Retirar dinero\n");
+        printf("4. Mas opciones\n");
+        printf("5. Salir y cerrar sesion\n");
+        scanf("%d", &opcion);
 
-            if (retiro > SALDO_INICIAL)
-            {
-             printf("Usted no cuenta con el dinero suficiente para realizar esta opcion");
-            }
-            break;
-
-        case 4:
-            printf("----Salir y cerrar sesion----\t");
-            printf("Gracias por usar CodexBank, su banco de confianzza\n");
-            printf("Cerrando sesion...\n");
-            return;
-            break;
-
-        case 5:
-
-            printf("----Mas opciones----\t");
-            printf("1. Cambiar su pin\n");
-            printf("2. Registrar su numero de telefono\n");
-            printf("3. Generar un estado de cuenta detallado\n");
-            printf("4. Regresar al menu principal\n");
-
-            switch (masOpciones)
-            {
+        switch (opcion) {
             case 1:
-                /* code */
+                consultarSaldo();
                 break;
             case 2:
-                /* code */
+                mostrarTransaccionesRecientes();
                 break;
             case 3:
-
+                retirarDinero();
                 break;
             case 4:
+                printf("----Mas opciones----\n");
+                printf("1. Cambiar su pin\n");
+                printf("2. Registrar su numero de telefono\n");
+                printf("3. Generar un estado de cuenta detallado\n");
+                scanf("%d", &masOpciones);
 
+                switch (masOpciones)
+                {
+                    case 1:
+                        printf("Ingrese su nuevo pin: ");
+                        scanf("%d", &nuevoPin);
+                        pin_valido = nuevoPin;
+                        printf("Su pin ha sido cambiado exitosamente.\n");
+                        break;
+
+                    case 2:
+                        printf("Registrar su numero de telefono\n");
+                        printf("Por favor ingrese su numero de telefono: ");
+                        scanf("%s", numeroTelefono);
+                        printf("Su numero de telefono ha sido registrado exitosamente!!!\n");
+                        break;
+
+                    case 3:
+                        printf("Generar un estado de cuenta detallado\n");
+                        printf("Nombre: %s\t Saldo actual %.2f\t Numero Telefonico %s\t", NOMBRE_VALIDO, saldo, numeroTelefono);
+                        printf("Transacciones recientes: \n");
+                        //Pendiente
+                        printf("Estado de cuenta detallado generado exitosamente!!!\n");
+                        break;
+                }
                 break;
-            
+            case 5:
+                printf("Saliendo y cerrando sesion\n");
+                break;
             default:
+                printf("Opcion no valida\n");
                 break;
-            }
-            break;
-
-        default:
-            printf("Por favor seleccione una opcion dentro del rango (1-5)");
-            return;
-            break;
-            
-    }
+        }
+    } while (opcion != 5);
 }
-
-void consultarSaldo(float saldo) {
-    
-}
-
-void mostrarTransaccionesRecientes(float transacciones[], int numTransacciones) {
-    
-}
-
-void retirarDinero(float *saldo, float transacciones[], int *numTransacciones) {
-    
-}
-
 
 int main() {
     char nombre[50];
-    char numeroTarjeta[50];
+    char numeroTarjeta[20];
     int pin;
-    float saldo = 50000;
+    int opcion;
+    int masOpciones;
+    int nuevoPin;
+    char numeroTelefono[20];
 
-    //Llamar a las funciones
-    void capturarDatos(char nombre[], char numeroTarjeta[], int pin);
+    capturarDatos(nombre, numeroTarjeta, pin);
+    mostrarMenu(opcion, masOpciones, nuevoPin, numeroTelefono);
 
     return 0;
 }
